dfbabba2eac44661d659b9d27776a1e21da83163
DaDDy <blacklistuser420@gmail.com>
2022-10-10 12:12:24 +0630

    helmet set up

--- security-example/server.js
+++ security-example/server.js
@@ -1,17 +1,29 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
+const express = require("express");
+const helmet = require("helmet");
 const app = express();
 const PORT = 3000;
 
+app.use(helmet());
 
-app.get('/secret',(req,res)=>{
-  res.send('Your personal secret value is 42!')
-})
+app.get("/secret", (req, res) => {
+  res.send("Your personal secret value is 42!");
+});
 
-app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname,'public','index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on PORT:${PORT}`)
-})
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`);
+  });
