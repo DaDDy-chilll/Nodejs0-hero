feed4540d4803e83ed0eda9ab2e685fc6128077a
DaDDy <blacklistuser420@gmail.com>
2022-11-16 13:19:39 +0630

    graphql start

---
+++ GraphQl/server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+const schema = buildSchema(`
+    type Query{
+      products:[Product]
+      orders:[Order]
+    }
+    type Product{
+        id: ID!
+        description:String!
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review{
+        rating:Int!
+        comment:String
+    }
+
+    type Order{
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
+    }
+`);
+const root = {
+  products: [
+    {
+      id: "redshoe",
+      description: "Red Shoe",
+      price: 43.12,
+    },
+    {
+      id: "bluejean",
+      description: "Blue Jeans",
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: "2005-05-05",
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: "redshoe",
+            description: "Old Red Shoe",
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
+};
+
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(8000, () => {
+  console.log("Running GraphQl server...");
+});
